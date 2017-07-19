169601a0b9617ed5b49e8b087131542228d31667
hollisneel <13ncn@uga.edu>
2017-07-18 22:19:57 -0400

    added l2cap

--- l2cap/l2cap-client.py
+++ l2cap/l2cap-client.py
@@ -1,9 +1,10 @@
-
 import bluetooth
 
 sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
-bd_addr = "01:23:45:67:89:AB"
+#bd_addr ="C8:F7:33:12:2B:53" # Hollis laptop
+bd_addr ="28:18:78:48:90:7C" # Hollis surface
+
 port = 0x1001
 
 sock.connect((bd_addr, port))
