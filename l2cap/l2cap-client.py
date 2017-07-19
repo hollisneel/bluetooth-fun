049329b8e1eee73abeca794eaab59615a6056530
hollisneel <13ncn@uga.edu>
2017-07-19 09:41:02 -0400

    added MAC addresses

--- l2cap/l2cap-client.py
+++ l2cap/l2cap-client.py
@@ -4,6 +4,8 @@ sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
 #bd_addr ="C8:F7:33:12:2B:53" # Hollis laptop
 bd_addr ="28:18:78:48:90:7C" # Hollis surface
+#bd_addr ="B8:27:EB:05:DD:D3" #pi
+
 
 port = 0x1001
 
